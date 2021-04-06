3c8944b967f9274681cf4e6f43761a45fdaabbb7
Johar Ali <50805982+JoharPiaic@users.noreply.github.com>
2021-04-06 23:19:18 +0500

    Nested Loop

---
+++ app.js
@@ -0,0 +1,37 @@
+// // alert("Hello World")
+
+// for(var a = 0; a<=10; a++)
+// document.write(a + "  " + "<");
+
+
+// for(a=1; a<5; a++)
+// {
+//     for(b=1; b<a; b++){
+
+//         document.write(b + "  " + "  ")
+//     }
+//     document.write(a + "<br>")
+// }
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+
+
+
+
+// for(a=1; a<=5; a++)
+// {
+//     for(b=1; b<=a; b++){
+
+//         document.write(b + "  ")
+//     }
+//     document.write( " <br>  ")
+// }
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
